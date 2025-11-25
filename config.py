570aecaee84503d4b8d1cf1498586194734b93d7
Zaaaza13 <63474590+Ilkhomjon13@users.noreply.github.com>
2025-11-25 15:10:49 +0500

    Update and rename config.py to Dockerfile

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-
-TOKEN = os.getenv("BOT_TOKEN")  # Telegram bot token
-ADMIN_ID = int(os.getenv("ADMIN_ID", "123456789"))  # Sizning ID
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:parol@host:port/dbname"
-)
