438785d99e18c5125ce27b2aed91582ded0b6819
Zaaaza13 <63474590+Ilkhomjon13@users.noreply.github.com>
2025-11-25 14:28:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 TOKEN = os.getenv("7196045219:AAFfbeIZQXKAb_cgAC2cnbdMY__L0Iakcrg")
 ADMIN_ID = int(os.getenv("1262207928", "0"))
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/sorovnoma")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:jnbJgLWFGsagpbhiQqMVWkXgNitEtmSz@postgres.railway.internal:5432/railway")
